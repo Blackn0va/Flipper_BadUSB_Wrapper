01b56a99f66ca0a0ade3e2bdbab1dba73e7bdefc
blacknovaa@gmail.com <blacknovaa@gmail.com>
2022-10-22 23:54:43 +0200

    Projektdateien hinzufügen.

---
+++ Powershell Wrapper/frmHauptprogramm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Powershell_Wrapper
+{
+    partial class frmHauptprogramm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(448, 43);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmHauptprogramm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 226);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmHauptprogramm";
+            this.Text = "Form1";
+             this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Powershell Wrapper/frmHauptprogramm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Powershell_Wrapper
+{
+    public partial class frmHauptprogramm : Form
+    {
+        System.IO.StreamWriter file;
+            
+        public frmHauptprogramm()
+        {
+            InitializeComponent();
+        }
+        
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //get filepath of dropped item and add it to the listbox
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            listBox1.Items.Add(files[0]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Datei = listBox1.Items[0].ToString();
+            Datei = Datei.Replace(".txt", "_flip.txt");
+
+
+
+ 
+            //open file from listbox an read line for line
+            file = new System.IO.StreamWriter(Datei, true);
+            file.WriteLine("DELAY 2000");
+            file.WriteLine("GUI r");
+            file.WriteLine("DELAY 1000");
+            file.WriteLine("STRING powershell");
+            file.WriteLine("DELAY 500");
+            file.WriteLine("ENTER");
+            file.WriteLine("DELAY 2000");
+            file.WriteLine("STRING cls");
+            file.WriteLine("ENTER");
+            file.Close();
+
+
+            string[] lines = System.IO.File.ReadAllLines(listBox1.Items[0].ToString());
+            foreach (string line in lines)
+            {
+                //make sure the line is not empty
+                if (line != "")
+                {
+                    //split the line into two parts
+                    string[] parts = line.Split(';');
+
+                    file = new System.IO.StreamWriter(Datei, true);
+                    file.WriteLine("STRING " + line);               
+                    file.WriteLine("ENTER");               
+                    file.Close();
+                }
+
+
+            }
+                     
+        }
+
+    }
+}
