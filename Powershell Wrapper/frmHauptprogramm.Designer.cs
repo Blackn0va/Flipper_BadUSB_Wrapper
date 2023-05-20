f5b35597ff5a4e8d087106a42648bb65781b4602
Blackn0va <blacknovaa@gmail.com>
2023-05-20 15:26:36 +0200

    Fixed Setup Files

--- Powershell Wrapper/frmHauptprogramm.Designer.cs
+++ Powershell Wrapper/frmHauptprogramm.Designer.cs
@@ -66,6 +66,7 @@
             this.ClientSize = new System.Drawing.Size(454, 132);
             this.Controls.Add(this.cmdConvert);
             this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(470, 171);
             this.MinimumSize = new System.Drawing.Size(470, 171);
