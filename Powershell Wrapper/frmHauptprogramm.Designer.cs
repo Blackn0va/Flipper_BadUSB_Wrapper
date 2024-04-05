0515b05ac8521292d2a673007d13943373259460
Blackn0va <blacknovaa@gmail.com>
2024-04-05 19:27:17 +0200

    c

--- Powershell Wrapper/frmHauptprogramm.Designer.cs
+++ Powershell Wrapper/frmHauptprogramm.Designer.cs
@@ -19,7 +19,7 @@
             }
             //nnnc
             base.Dispose(disposing);
-            //ssss
+            //ssssc
         }
 
         #region Vom Windows Form-Designer generierter Code
